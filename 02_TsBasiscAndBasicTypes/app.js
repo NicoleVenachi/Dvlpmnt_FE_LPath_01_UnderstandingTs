a830c84b133974bd75538f5e02db826fa1eb712f
nicolevenachi <ludwing@unicauca.edu.co>
2024-08-24 20:55:13 -0500

    02_TsBasicsAndTypes->TsVsJsTypes

--- 02_TsBasiscAndBasicTypes/app.js
+++ 02_TsBasiscAndBasicTypes/app.js
@@ -1,9 +1,11 @@
 function add(n1, n2) {
-    return n1 + n2; // without ts, js assumes the result is maintains the first element type
+    console.log(typeof num1);
+    // //vanilla js
+    // if (typeof n1 !== 'number' || typeof n2 !== 'number') throw new Error('Incorrect input');
+    return n1 + n2;
 }
 var num1 = 5;
-// const num1: number = '5'; // result = '52.8'
-var num2 = '3.8';
-// const num2: number = '3.8'; // result = 8.8
+// const num1: number = '5'; // result = '52.8'. Without ts, js assumes that since there is a string, the other should be converted to a number before operated it
+var num2 = 3.8;
 var result = add(num1, num2);
 console.log(result);
