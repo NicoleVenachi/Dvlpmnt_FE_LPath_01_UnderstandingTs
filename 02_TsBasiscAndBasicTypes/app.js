43148ac4dc1578b17ecd8d6deb29e46b1f67f0e4
nicolevenachi <ludwing@unicauca.edu.co>
2024-08-31 22:06:17 -0500

    02_TsBasicsAndTypes->03_Numbers,StringsAndBooleans

--- 02_TsBasiscAndBasicTypes/app.js
+++ 02_TsBasiscAndBasicTypes/app.js
@@ -1,11 +1,17 @@
-function add(n1, n2) {
+function add(n1, n2, showResult, phrase) {
     console.log(typeof num1);
     // //vanilla js
     // if (typeof n1 !== 'number' || typeof n2 !== 'number') throw new Error('Incorrect input');
-    return n1 + n2;
+    if (showResult) {
+        console.log("".concat(phrase, " ").concat(n1 + n2));
+    }
+    else {
+        return n1 + n2;
+    }
 }
 var num1 = 5;
 // const num1: number = '5'; // result = '52.8'. Without ts, js assumes that since there is a string, the other should be converted to a number before operated it
 var num2 = 3.8;
-var result = add(num1, num2);
-console.log(result);
+var printResult = true;
+var resultPhrase = 'Result is: \t';
+add(num1, num2, printResult, resultPhrase);
--- 02_TsBasiscAndBasicTypes/app.ts
+++ 02_TsBasiscAndBasicTypes/app.ts
@@ -1,18 +1,23 @@
 
-function add(n1:number, n2:number) {
+function add(n1:number, n2:number, showResult: boolean, phrase:string):void|number {
   console.log(typeof num1);
 
   // //vanilla js
   // if (typeof n1 !== 'number' || typeof n2 !== 'number') throw new Error('Incorrect input');
    
-  return n1 + n2; 
+  if (showResult) {
+    console.log(`${phrase} ${n1+n2}`);
+  } 
+  else {
+    return n1 + n2; 
+  }
 }
 
 const num1: number = 5;
 // const num1: number = '5'; // result = '52.8'. Without ts, js assumes that since there is a string, the other should be converted to a number before operated it
 
 const num2: number = 3.8; 
+const printResult = true;
+const resultPhrase = 'Result is: \t';
 
-const result: number = add(num1, num2);
-
-console.log(result);
+add(num1, num2, printResult, resultPhrase);
